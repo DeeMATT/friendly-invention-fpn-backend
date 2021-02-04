3543530bac9032450b148d75edc78c24aff3ca89
DeeMATT <mattode@outlook.com>
2021-02-04 17:52:24 +0100

    update

--- FPN_Backend/FPN_Backend/settings/base.py
+++ FPN_Backend/FPN_Backend/settings/base.py
@@ -17,13 +17,15 @@ from dotenv import load_dotenv
 
 from pathlib import Path
 
-# PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# BASE_DIR = os.path.dirname(PROJECT_DIR)
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 # ENV_PATH = os.path.join(BASE_DIR, '.env')
 print("bajanijowdokd", BASE_DIR)
+print("gbeduh39212e", PROJECT_DIR)
+return
 ENV_PATH = BASE_DIR / '.env'
 load_dotenv(ENV_PATH)
 
