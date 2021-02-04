b5320cbdd3b2fddf0b7b8a6bf54302ab39fb426c
DeeMATT <mattode@outlook.com>
2021-02-04 04:26:23 +0100

    provisionUser()

--- FPN_Backend/users/views.py
+++ FPN_Backend/users/views.py
@@ -1,57 +1,70 @@
 from django.shortcuts import render
 from django.conf import settings
-from api_utils.views import (
-    badRequestResponse, InternalServerError, successResponse, createdResponse,
+from errors.views import getError, ErrorCodes
+from api_utility.views import (
+    badRequestResponse, internalServerError, successResponse, createdResponse,
     unAuthenticatedResponse, unAuthorizedResponse, resourceConflictResponse, 
     resourceNotFoundResponse, paginatedResponse, getUserIpAddress
 )
+from api_utility.validators import (
+    validateKeys, validateEmailFormat, validatePhoneFormat, 
+    validateThatStringIsEmptyAndClean, validateThatStringIsEmpty
+)
+from .utils import (
+    getUserByEmail, getUserById, getUserByPhone, getUserByUsername, createUser
+)
+from data_transformer.serializers import transformUser
+import json
+import logging
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 
 def provisionUser(request):
     # check for root secret in header
     secret = request.headers.get('Secret')
     if not secret == settings.SECRET:
-        return unAuthorizedResponse(errorCode=ErrorCodes.INVALID_CREDENTIALS, message=getInvalidCredentialsErrorPacket())
+        return unAuthorizedResponse(getError(ErrorCodes.UNAUTHORIZED_REQUEST, "Invalid Secret Key"))
 
     ## decode the request body
     body = json.loads(request.body)
 
     # check if required fields are present in request payload
-    missingKeys = validateKeys(payload=body, requiredKeys=['email', 'firstName', 'lastName', 'userName', 'password', 'phone'])
+    missingKeys = validateKeys(payload=body, requiredKeys=['email', 'firstName', 'lastName', 'username', 'password', 'phone'])
     if missingKeys:
-        return badRequestResponse(ErrorCodes.MISSING_FIELDS, message=f"The following key(s) are missing in the request payload: {missingKeys}")
+        return badRequestResponse(getError(ErrorCodes.MISSING_FIELDS, f"The following key(s) are missing in the request payload: {missingKeys}"))
 
     # validate if the email is in the correct format
     if not validateEmailFormat(body['email']):
-        return badRequestResponse(errorCode = ErrorCodes.GENERIC_INVALID_PARAMETERS, message = "Email format is invalid")
+        return badRequestResponse(getError(ErrorCodes.GENERIC_ERROR, message="Email format is invalid")
     
     if not validateThatStringIsEmptyAndClean(body['firstName']):
-        return badRequestResponse(errorCode = ErrorCodes.GENERIC_INVALID_PARAMETERS, message = "First name can not contain special characters")
+        return badRequestResponse(getError(ErrorCodes.GENERIC_ERROR, message="First name can not contain special characters")
     
     if not validateThatStringIsEmptyAndClean(value=body['lastName']):
-        return badRequestResponse(errorCode = ErrorCodes.GENERIC_INVALID_PARAMETERS, message = "Last name can not contain special characters")
-    
-    if not validateThatAStringIsClean(body['userName']):
-        return badRequestResponse(errorCode = ErrorCodes.GENERIC_INVALID_PARAMETERS, message = "User name can not contain special characters")
+        return badRequestResponse(getError(ErrorCodes.GENERIC_ERROR, message="Last name can not contain special characters")
 
     # check if user with that email exists
-    if getUserByEmail(body['email'], ) is not None:
-        return resourceConflictResponse(errorCode=ErrorCodes.USER_ALREADY_EXIST, 
-                                message = getUserAlreadyExistErrorPacket(value='email'))
+    if getUserByEmail(body['email']):
+        return resourceConflictResponse(getError(ErrorCodes.USER_ALREADY_EXISTS, message="A user already exists with same email"))
 
     # check if user with that username exists
-    if getUserByUserName(body['userName'], ) is not None:
-        return resourceConflictResponse(errorCode=ErrorCodes.USER_ALREADY_EXIST, 
-                                message = getUserAlreadyExistErrorPacket(value='username'))
+    if getUserByUsername(body['username']) is not None:
+        return resourceConflictResponse(getError(ErrorCodes.USER_ALREADY_EXISTS, message="A user already exists with same username"))
 
     ## check if user with that phone exists
-    if getUserByPhone(body['phone'], ) is not None:
-        return resourceConflictResponse(errorCode=ErrorCodes.USER_ALREADY_EXIST, 
-                                message = getUserAlreadyExistErrorPacket(value='phone'))
+    if getUserByPhone(body['phone']) is not None:
+        return resourceConflictResponse(getError(ErrorCodes.USER_ALREADY_EXISTS, message="A user already exists with same phone"))
 
     # create userProfile
-    user = createUserRecord(firstName=body['firstName'], lastName=body['lastName'], userName=body['userName'], 
-                            email=body['email'], phone=body['phone'], password=body['password'])
+    user = createUser(
+                firstName=body['firstName'], 
+                lastName=body['lastName'], 
+                username=body['userName'], 
+                email=body['email'], 
+                phone=body['phone'], 
+                password=body['password']
+            )
     
-    if user is not None:
-        return successResponse(message="successfully created", body=transformUser(user))
+    if user:
+        return createdResponse(message="successfully created", body=transformUser(user))
