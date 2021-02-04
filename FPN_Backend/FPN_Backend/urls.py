7cc645dc53dbaf64989739c6da4b62d927b13441
DeeMATT <mattode@outlook.com>
2021-02-04 05:03:04 +0100

    user provision URL

--- FPN_Backend/FPN_Backend/urls.py
+++ FPN_Backend/FPN_Backend/urls.py
@@ -7,6 +7,7 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
+from users.views import provisionUser
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
@@ -15,6 +16,7 @@ urlpatterns = [
     path('documents/', include(wagtaildocs_urls)),
 
     path('search/', search_views.search, name='search'),
+    path('users/provision', provisionUser),
 
 ]
 
